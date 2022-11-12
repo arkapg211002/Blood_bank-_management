6460e95760682b615ac1c064fd2ad02214aac2e2
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2022-11-12 13:51:11 +0530

    first commit

---
+++ blood_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from blood_app.models import *
+# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Blood_Donation)
+admin.site.register(Order)
